1779515c46a92d96a2540011f8eff8274d341a4a
Annie Tang <annietang@berkeley.edu>
2017-09-15 09:46:22 -0700

    added in all the images and names + the random image/name selection

---
+++ trivia-app/GameViewController.swift
@@ -0,0 +1,91 @@
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    let darkBlue: UIColor = UIColor(red:0.20, green:0.35, blue:0.52, alpha:1.0)
+    let lightBlue: UIColor = UIColor(red:0.38, green:0.59, blue:0.83, alpha:1.0)
+    let lightGray: UIColor = UIColor(red:0.97, green:0.97, blue:0.97, alpha:1.0)
+    var darkGrayBlue: UIColor = UIColor(red:0.19, green:0.24, blue:0.44, alpha:1.0)
+    
+    var score: Int!
+    var scoreLabel: UILabel!
+    
+    var promptLabel: UILabel!
+    
+    var randomPair: (UIImage, String)!
+    var memberImage: UIImageView!
+    var memberName: String!
+    
+    var choiceArr: [UIButton]!
+    var choiceOne: UIButton!
+    var choiceTwo: UIButton!
+    var choiceThree: UIButton!
+    var choiceFour: UIButton!
+    
+    var imageArr: [UIImage]!
+    var nameArr: [String]!
+    
+    override func viewDidLoad() {
+        
+        view.backgroundColor = lightGray
+        setUpArrays()
+        score = 0
+        
+        promptLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * 0.5))
+        promptLabel.text = "Which member is this?"
+        promptLabel.textColor = darkBlue
+        promptLabel.textAlignment = .center
+        promptLabel.font = UIFont(name: promptLabel.font.fontName, size: 30)
+        view.addSubview(promptLabel)
+        
+        
+        scoreLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * 0.6))
+        scoreLabel.text = "YOUR SCORE: \(score!)"
+        scoreLabel.textAlignment = .center
+        scoreLabel.textColor = darkGrayBlue
+        scoreLabel.font = UIFont(name: scoreLabel.font.fontName, size: 20)
+        view.addSubview(scoreLabel)
+        
+        randomPair = getImage()
+        memberName = randomPair.1
+        memberImage = UIImageView(frame: CGRect(x: view.frame.width * 0.375, y: view.frame.height * 0.35, width: view.frame.width * 0.25, height: view.frame.height * 0.25))
+        memberImage.image = randomPair.0
+        memberImage.contentMode = .scaleAspectFill
+        view.addSubview(memberImage)
+        
+        //        choiceOne.setTitle(memberName, for: .normal)
+        
+        setUpChoices()
+        
+    }
+    
+    func setUpChoices() {
+        choiceOne = UIButton(frame: CGRect(x: view.frame.width * 0.15, y: view.frame.height * 0.6, width: view.frame.width * 0.7, height: 50))
+        choiceTwo = UIButton(frame: CGRect(x: view.frame.width * 0.15, y: view.frame.height * 0.65, width: view.frame.width * 0.7, height: 50))
+        choiceThree = UIButton(frame: CGRect(x: view.frame.width * 0.15, y: view.frame.height * 0.70, width: view.frame.width * 0.7, height: 50))
+        choiceFour  = UIButton(frame: CGRect(x: view.frame.width * 0.15, y: view.frame.height * 0.75, width: view.frame.width * 0.7, height: 50))
+        
+        choiceArr = [choiceOne, choiceTwo, choiceThree, choiceFour]
+
+        for i in 0...3 {
+            let choice = choiceArr[i]
+            choice.setTitleColor(darkBlue, for: .normal)
+            view.addSubview(choice)
+        }
+    }
+    
+    func getImage() -> (UIImage, String) {
+        let imgRange: UInt32 = UInt32(imageArr.count)
+        let randomNumber = Int(arc4random_uniform(imgRange))
+        let randomImage: UIImage = imageArr[randomNumber]
+        let randomName: String = nameArr[randomNumber]
+        return (randomImage, randomName)
+    }
+    
+    func setUpArrays()  {
+        imageArr = [#imageLiteral(resourceName: "aayushtyagi"), #imageLiteral(resourceName: "abhinavkoppu"), #imageLiteral(resourceName: "adhirajdatar"), #imageLiteral(resourceName: "akkshaykhoslaa"), #imageLiteral(resourceName: "amyshen"), #imageLiteral(resourceName: "aneeshjindal"), #imageLiteral(resourceName: "ashwinvaidyanathan"), #imageLiteral(resourceName: "bengoldberg"), #imageLiteral(resourceName: "billylu"), #imageLiteral(resourceName: "borisyue"), #imageLiteral(resourceName: "candiceye"), #imageLiteral(resourceName: "codyhsieh"), #imageLiteral(resourceName: "danielandrews"), #imageLiteral(resourceName: "edwardliu"), #imageLiteral(resourceName: "eliothan"), #imageLiteral(resourceName: "emaanhariri"), #imageLiteral(resourceName: "erickong"), #imageLiteral(resourceName: "jaredgutierrez"), #imageLiteral(resourceName: "jeffreyzhang"), #imageLiteral(resourceName: "jessicachen"), #imageLiteral(resourceName: "julialuo"), #imageLiteral(resourceName: "justinkim"), #imageLiteral(resourceName: "kevinjiang"), #imageLiteral(resourceName: "krishnanrajiyah"), #imageLiteral(resourceName: "kristinho"), #imageLiteral(resourceName: "leonkwak"), #imageLiteral(resourceName: "leviwalsh"), #imageLiteral(resourceName: "mohitkatyal"), #imageLiteral(resourceName: "muditmittal"), #imageLiteral(resourceName: "peterschafhalter"), #imageLiteral(resourceName: "radhikadhomse"), #imageLiteral(resourceName: "rochelleshen"), #imageLiteral(resourceName: "sahillamba"), #imageLiteral(resourceName: "sarahtang"), #imageLiteral(resourceName: "sayanpaul"), #imageLiteral(resourceName: "shariewang"), #imageLiteral(resourceName: "shivkushwah"), #imageLiteral(resourceName: "shreyareddy"), #imageLiteral(resourceName: "sumukhshivakumar"), #imageLiteral(resourceName: "tarunkhasnavis"), #imageLiteral(resourceName: "victorsun"), #imageLiteral(resourceName: "wilburshi"), #imageLiteral(resourceName: "younglin"), #imageLiteral(resourceName: "zachgovani")]
+        
+        nameArr = ["Aayush Tyagi", "Abhinav Koppu", "Adhiraj Datar", "Akkshay Khoslaa", "Amy Shen", "Aneesh Jindal", "Ashwin Vaidyanathan", "Ben Goldberg", "Billy Lu", "Boris Yue", "Edward Liu", "Candice Ye", "Cody Hsieh", "Daniel Andrews", "Eliot Han", "Emaan Hariri", "Eric Kong", "Jared Gutierrez", "Jeffrey Zhang", "Jessica Chen", "Julia Luo", "Justin Kim", "Kevin Jiang", "Krishnan Rajiyah", "Kristin Ho", "Leon Kwak", "Levi Walsh", "Mohit Katyal", "Mudit Mittal", "Peter Schafhalter", "Radhika Dhomse", "Rochelle Shen", "Sahil Lamba", "Sarah Tang", "Sayan Paul", "Sharie Wang", "Shiv Kushwah", "Shreya Reddy", "Shubham Goenka", "Sumukh Shivakumar", "Tarun Khasnavis", "Victor Sun", "Vidya Ravikumar", "Wilbur Shi", "Young Lin", "Zach Govani"];
+
+        }
+    }
--- trivia-app/ViewController.swift
+++ trivia-app/ViewController.swift
@@ -1,18 +1,46 @@
-//
-//  ViewController.swift
-//  trivia-app
-//
-//  Created by Annie Tang on 9/15/17.
-//  Copyright © 2017 Annie Tang. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let darkBlue: UIColor = UIColor(red:0.20, green:0.35, blue:0.52, alpha:1.0)
+    let lightBlue: UIColor = UIColor(red:0.38, green:0.59, blue:0.83, alpha:1.0)
+    
+    var startButton: UIButton!
+    var titleLabel: UILabel!
+    var subtitleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = lightBlue
+        
+        startButton = UIButton(frame: CGRect(x: view.frame.width * 0.15, y: view.frame.height * 0.53, width: view.frame.width * 0.7, height: 50))
+        startButton.setTitle("START", for: .normal)
+        startButton.setTitleColor(UIColor.white, for: .normal)
+        startButton.layer.borderWidth = 1
+        startButton.layer.borderColor = UIColor.white.cgColor
+        startButton.addTarget(self, action: #selector(startGame), for: .touchUpInside)
+        view.addSubview(startButton)
+        
+        titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * 0.8))
+        titleLabel.text = "MDB TRIVIA"
+        titleLabel.textColor = UIColor.white
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont(name: titleLabel.font.fontName, size: 50)
+        view.addSubview(titleLabel)
+        
+        subtitleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * 0.94))
+        subtitleLabel.text = "test your knowledge."
+        subtitleLabel.textColor = UIColor.white
+        subtitleLabel.textAlignment = .center
+        subtitleLabel.font = UIFont(name: titleLabel.font.fontName, size: 20)
+        view.addSubview(subtitleLabel)
+
+    }
+    
+    func startGame() {
+        startButton.backgroundColor = darkBlue
+        self.performSegue(withIdentifier: "toGame", sender: self)
     }
 
     override func didReceiveMemoryWarning() {
