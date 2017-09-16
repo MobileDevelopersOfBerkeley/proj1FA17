da51e014bc7605ff79001ce9d38d74ae2ca5a018
Annie Tang <annietang@berkeley.edu>
2017-09-16 00:18:16 -0700

    fixed bug for increment by one and also same name reappearing

--- trivia-app/GameViewController.swift
+++ trivia-app/GameViewController.swift
@@ -7,21 +7,24 @@ class GameViewController: UIViewController {
     let lightGray: UIColor = UIColor(red:0.97, green:0.97, blue:0.97, alpha:1.0)
     var darkGrayBlue: UIColor = UIColor(red:0.19, green:0.24, blue:0.44, alpha:1.0)
     
-    var score: Int!
+    var score: Int = 0
     var scoreLabel: UILabel!
     
     var promptLabel: UILabel!
     
-    var randomPair: (UIImage, String)!
+    var randomTuple: (UIImage, String, Int)!
     var memberImage: UIImageView!
     var memberName: String!
+    var memberNumber: Int!
     
     var choiceArr: [UIButton]!
     var choiceOne: UIButton!
     var choiceTwo: UIButton!
     var choiceThree: UIButton!
     var choiceFour: UIButton!
-    var correctChoice: UIButton!
+    
+//    var correctChoice: UIButton!
+//    var currentChoice: UIButton!
     
     var imageArr: [UIImage]!
     var nameArr: [String]!
@@ -30,7 +33,6 @@ class GameViewController: UIViewController {
         
         view.backgroundColor = lightGray
         setUpArrays()
-        score = 0
         
         promptLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * 0.5))
         promptLabel.text = "Which member is this?"
@@ -41,70 +43,113 @@ class GameViewController: UIViewController {
         
         
         scoreLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * 0.6))
-        scoreLabel.text = "YOUR SCORE: \(score!)"
+        scoreLabel.text = "YOUR SCORE: \(score)"
         scoreLabel.textAlignment = .center
         scoreLabel.textColor = darkGrayBlue
         scoreLabel.font = UIFont(name: scoreLabel.font.fontName, size: 20)
         view.addSubview(scoreLabel)
-        
-        randomPair = getImage()
-        memberImage.image = randomPair.0
-        memberName = randomPair.1
-        
-        memberImage.contentMode = .scaleAspectFill
+    
         memberImage = UIImageView(frame: CGRect(x: view.frame.width * 0.375, y: view.frame.height * 0.35, width: view.frame.width * 0.25, height: view.frame.height * 0.25))
         view.addSubview(memberImage)
         
-        initializeChoices()
-        
-    }
-    
-    func initializeChoices() {
         choiceOne = UIButton(frame: CGRect(x: view.frame.width * 0.15, y: view.frame.height * 0.6, width: view.frame.width * 0.7, height: 50))
         choiceTwo = UIButton(frame: CGRect(x: view.frame.width * 0.15, y: view.frame.height * 0.65, width: view.frame.width * 0.7, height: 50))
         choiceThree = UIButton(frame: CGRect(x: view.frame.width * 0.15, y: view.frame.height * 0.70, width: view.frame.width * 0.7, height: 50))
         choiceFour  = UIButton(frame: CGRect(x: view.frame.width * 0.15, y: view.frame.height * 0.75, width: view.frame.width * 0.7, height: 50))
         
         choiceArr = [choiceOne, choiceTwo, choiceThree, choiceFour]
-
-        for i in 0...3 {
-            let choice = choiceArr[i]
-            choice.setTitleColor(darkBlue, for: .normal)
+        for i in 0 ..< 4 {
+            let choice: UIButton = choiceArr[i]
+            choice.setTitleColor(darkGrayBlue, for: .normal)
             view.addSubview(choice)
         }
+        nextMember()
     }
     
-    func populateChoices() {
-        var usedNames: [Int: String]!
+    
+    func nextMember() {
+        randomTuple = getImage()
         
-        let imgRange: UInt32 = UInt32(4)
-        let correctNumber = Int(arc4random_uniform(imgRange))
-        usedNames[correctNumber] = memberName
+        memberImage.image = randomTuple.0
+        memberImage.contentMode = .scaleAspectFill
+        
+        memberName = randomTuple.1
+        memberNumber = randomTuple.2
+        scoreLabel.text = "YOUR SCORE: \(score)"
+
+        populateChoices()
+    }
+    
+    func correctTap() {
+        print("INCREMENTED")
+        score += 1
+        scoreLabel.text = "YOUR SCORE: \(score)"
+        nextMember()
+    }
+    
+    /* creates a dictionary choiceNames to keep track of final buttons: memberNames
+       creates usedNumbers to keep track of which memberNumbers have been used
+       randomly picks a number from 0..3 to assign correctName to a button
+       puts the correctName into the dictionary, with the appropriate key
+       finds 3 other random choices, unduplicated, and also add them into dict
+     */
+    func populateChoices() {
+        var choiceNames = [Int: String]()
+        var usedImgNumbers = [Int]()
+        
+        let correctNumber = Int(arc4random_uniform(UInt32(4)))
+        print("correctNumber: ", correctNumber)
+        choiceNames[correctNumber] = memberName
+        usedImgNumbers.append(memberNumber)
         
         var randomNumber: Int!
+        let imgRange: UInt32 = UInt32(imageArr.count)
+        
+        
+        
         for i in 0...3 {
-            if usedNames[i] != nil {
+            if choiceNames[i] == nil {
+//                print(i)
                 randomNumber = Int(arc4random_uniform(imgRange))
-                usedNames[i]
-                
+                while usedImgNumbers.contains(randomNumber) {
+                    randomNumber = Int(arc4random_uniform(imgRange))
+                }
+//                print("random number: \(randomNumber)")
+                choiceNames[i] = nameArr[randomNumber]
+                usedImgNumbers.append(randomNumber)
+            }
+        }
+        
+        for (i, name) in choiceNames {
+            let choice = choiceArr[i]
+            choice.setTitle(name, for: .normal)
+            choice.removeTarget(nil, action: nil, for: .allEvents)
+//            print(i, name, correctNumber)
+            if i == correctNumber {
+//                print("correct: ", i)
+                choice.addTarget(self, action: #selector(correctTap), for: .touchUpInside)
+            } else {
+//                print("incorrect: ", i)
+                choice.addTarget(self, action: #selector(nextMember), for: .touchUpInside)
             }
         }
         
         
     }
     
-    func getImage() -> (UIImage, String) {
+    func getImage() -> (UIImage, String, Int) {
         let imgRange: UInt32 = UInt32(imageArr.count)
         let randomNumber = Int(arc4random_uniform(imgRange))
         let randomImage: UIImage = imageArr[randomNumber]
         let randomName: String = nameArr[randomNumber]
-        return (randomImage, randomName)
+        print(randomName)
+        return (randomImage, randomName,randomNumber)
     }
     
     func setUpArrays()  {
-        imageArr = [#imageLiteral(resourceName: "aayushtyagi"), #imageLiteral(resourceName: "abhinavkoppu"), #imageLiteral(resourceName: "adhirajdatar"), #imageLiteral(resourceName: "akkshaykhoslaa"), #imageLiteral(resourceName: "amyshen"), #imageLiteral(resourceName: "aneeshjindal"), #imageLiteral(resourceName: "ashwinvaidyanathan"), #imageLiteral(resourceName: "bengoldberg"), #imageLiteral(resourceName: "billylu"), #imageLiteral(resourceName: "borisyue"), #imageLiteral(resourceName: "candiceye"), #imageLiteral(resourceName: "codyhsieh"), #imageLiteral(resourceName: "danielandrews"), #imageLiteral(resourceName: "edwardliu"), #imageLiteral(resourceName: "eliothan"), #imageLiteral(resourceName: "emaanhariri"), #imageLiteral(resourceName: "erickong"), #imageLiteral(resourceName: "jaredgutierrez"), #imageLiteral(resourceName: "jeffreyzhang"), #imageLiteral(resourceName: "jessicachen"), #imageLiteral(resourceName: "julialuo"), #imageLiteral(resourceName: "justinkim"), #imageLiteral(resourceName: "kevinjiang"), #imageLiteral(resourceName: "krishnanrajiyah"), #imageLiteral(resourceName: "kristinho"), #imageLiteral(resourceName: "leonkwak"), #imageLiteral(resourceName: "leviwalsh"), #imageLiteral(resourceName: "mohitkatyal"), #imageLiteral(resourceName: "muditmittal"), #imageLiteral(resourceName: "peterschafhalter"), #imageLiteral(resourceName: "radhikadhomse"), #imageLiteral(resourceName: "rochelleshen"), #imageLiteral(resourceName: "sahillamba"), #imageLiteral(resourceName: "sarahtang"), #imageLiteral(resourceName: "sayanpaul"), #imageLiteral(resourceName: "shariewang"), #imageLiteral(resourceName: "shivkushwah"), #imageLiteral(resourceName: "shreyareddy"), #imageLiteral(resourceName: "sumukhshivakumar"), #imageLiteral(resourceName: "tarunkhasnavis"), #imageLiteral(resourceName: "victorsun"), #imageLiteral(resourceName: "wilburshi"), #imageLiteral(resourceName: "younglin"), #imageLiteral(resourceName: "zachgovani")]
+        imageArr = [#imageLiteral(resourceName: "aayushtyagi"), #imageLiteral(resourceName: "abhinavkoppu"), #imageLiteral(resourceName: "adhirajdatar"), #imageLiteral(resourceName: "akkshaykhoslaa"), #imageLiteral(resourceName: "amyshen"), #imageLiteral(resourceName: "aneeshjindal"), #imageLiteral(resourceName: "ashwinvaidyanathan"), #imageLiteral(resourceName: "bengoldberg"), #imageLiteral(resourceName: "billylu"), #imageLiteral(resourceName: "borisyue"), #imageLiteral(resourceName: "candiceye"), #imageLiteral(resourceName: "codyhsieh"), #imageLiteral(resourceName: "danielandrews"), #imageLiteral(resourceName: "edwardliu"), #imageLiteral(resourceName: "eliothan"), #imageLiteral(resourceName: "emaanhariri"), #imageLiteral(resourceName: "erickong"), #imageLiteral(resourceName: "jaredgutierrez"), #imageLiteral(resourceName: "jeffreyzhang"), #imageLiteral(resourceName: "jessicachen"), #imageLiteral(resourceName: "julialuo"), #imageLiteral(resourceName: "justinkim"), #imageLiteral(resourceName: "kevinjiang"), #imageLiteral(resourceName: "krishnanrajiyah"), #imageLiteral(resourceName: "kristinho"), #imageLiteral(resourceName: "leonkwak"), #imageLiteral(resourceName: "leviwalsh"), #imageLiteral(resourceName: "mohitkatyal"), #imageLiteral(resourceName: "muditmittal"), #imageLiteral(resourceName: "peterschafhalter"), #imageLiteral(resourceName: "radhikadhomse"), #imageLiteral(resourceName: "rochelleshen"), #imageLiteral(resourceName: "sahillamba"), #imageLiteral(resourceName: "sarahtang"), #imageLiteral(resourceName: "sayanpaul"), #imageLiteral(resourceName: "shariewang"), #imageLiteral(resourceName: "shivkushwah"), #imageLiteral(resourceName: "shreyareddy"),#imageLiteral(resourceName: "shubhamgoenka"), #imageLiteral(resourceName: "sumukhshivakumar"), #imageLiteral(resourceName: "tarunkhasnavis"), #imageLiteral(resourceName: "victorsun"), #imageLiteral(resourceName: "vidyaravikumar"), #imageLiteral(resourceName: "wilburshi"), #imageLiteral(resourceName: "younglin"), #imageLiteral(resourceName: "zachgovani")]
         
-        nameArr = ["Aayush Tyagi", "Abhinav Koppu", "Adhiraj Datar", "Akkshay Khoslaa", "Amy Shen", "Aneesh Jindal", "Ashwin Vaidyanathan", "Ben Goldberg", "Billy Lu", "Boris Yue", "Edward Liu", "Candice Ye", "Cody Hsieh", "Daniel Andrews", "Eliot Han", "Emaan Hariri", "Eric Kong", "Jared Gutierrez", "Jeffrey Zhang", "Jessica Chen", "Julia Luo", "Justin Kim", "Kevin Jiang", "Krishnan Rajiyah", "Kristin Ho", "Leon Kwak", "Levi Walsh", "Mohit Katyal", "Mudit Mittal", "Peter Schafhalter", "Radhika Dhomse", "Rochelle Shen", "Sahil Lamba", "Sarah Tang", "Sayan Paul", "Sharie Wang", "Shiv Kushwah", "Shreya Reddy", "Shubham Goenka", "Sumukh Shivakumar", "Tarun Khasnavis", "Victor Sun", "Vidya Ravikumar", "Wilbur Shi", "Young Lin", "Zach Govani"];
+        nameArr = ["Aayush Tyagi", "Abhinav Koppu", "Adhiraj Datar", "Akkshay Khoslaa", "Amy Shen", "Aneesh Jindal", "Ashwin Vaidyanathan", "Ben Goldberg", "Billy Lu", "Boris Yue", "Candice Ye", "Cody Hsieh", "Daniel Andrews", "Edward Liu", "Eliot Han", "Emaan Hariri", "Eric Kong", "Jared Gutierrez", "Jeffrey Zhang", "Jessica Chen", "Julia Luo", "Justin Kim", "Kevin Jiang", "Krishnan Rajiyah", "Kristin Ho", "Leon Kwak", "Levi Walsh", "Mohit Katyal", "Mudit Mittal", "Peter Schafhalter", "Radhika Dhomse", "Rochelle Shen", "Sahil Lamba", "Sarah Tang", "Sayan Paul", "Sharie Wang", "Shiv Kushwah", "Shreya Reddy", "Shubham Goenka", "Sumukh Shivakumar", "Tarun Khasnavis", "Victor Sun", "Vidya Ravikumar", "Wilbur Shi", "Young Lin", "Zach Govani"];
 
         }
     }
